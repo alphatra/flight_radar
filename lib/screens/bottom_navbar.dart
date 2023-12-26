16db3e9f1de93bc12412b83e2085b6c70181000f
alpphatra <alpphatra@gmail.com>
2023-12-26 22:16:19 +0100

    Tworzę w c++ listę losowych sparametryzowanych lotów

---
+++ lib/screens/bottom_navbar.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class BottomNavBar extends StatelessWidget {
+  final int selectedIndex;
+  final Function(int) onItemSelected;
+
+  const BottomNavBar({
+    Key? key,
+    required this.selectedIndex,
+    required this.onItemSelected,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return NavigationBar(
+      animationDuration: const Duration(milliseconds: 1000),
+      destinations: const <Widget>[
+        NavigationDestination(
+          icon: Icon(Icons.home),
+          label: 'Home',
+        ),
+        NavigationDestination(
+          icon: Icon(Icons.explore),
+          label: 'Explore',
+        ),
+        NavigationDestination(
+          icon: Icon(Icons.person),
+          label: 'Profile',
+        ),
+        NavigationDestination(
+          icon: Icon(Icons.settings_rounded),
+          label: 'Settings',
+        ),
+      ],
+      onDestinationSelected: onItemSelected,
+      selectedIndex: selectedIndex,
+    );
+  }
+}

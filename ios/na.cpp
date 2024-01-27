45165b0ab73b4bc56d73ddcb858e8151fc03d8a1
alpphatra <alpphatra@gmail.com>
2024-01-27 19:53:09 +0100

    Naprawa wyświetlania danych lotu w FlightListView

--- ios/na.cpp
+++ ios/na.cpp
@@ -6,7 +6,7 @@
 extern "C" __attribute__((visibility("default"))) __attribute__((used))
 const char* fetchFlights() {
     try {
-        http::Request request{"http://srv19.mikr.us:20322/flights"};
+        http::Request request{"http://192.168.100.4:8080/flights"};
         const auto response = request.send("GET");
         char* cstr = new char[response.body.size() + 1];
         std::strcpy(cstr, std::string(response.body.begin(), response.body.end()).c_str());
@@ -21,7 +21,7 @@ extern "C" __attribute__((visibility("default"))) __attribute__((used))
 const char* fetchFlightsFromCity(const char* cityName) {
     try {
         // Tworzenie URL na podstawie nazwy miasta
-        std::string url = "http://srv19.mikr.us:20322/flights/fromCity/" + std::string(cityName);
+        std::string url = "http://192.168.100.4:8080/flights/fromCity/" + std::string(cityName);
 
         // Wykonanie zapytania HTTP GET
         http::Request request{url};
@@ -32,6 +32,42 @@ const char* fetchFlightsFromCity(const char* cityName) {
         std::strcpy(cstr, std::string(response.body.begin(), response.body.end()).c_str());
         return cstr;
 
+    } catch (const std::exception& e) {
+        printf("Request failed, error: %s\n", e.what());
+        return nullptr;
+    }
+}
+
+extern "C" __attribute__((visibility("default"))) __attribute__((used))
+const char* fetchFlightsFromTo(const char* fromCity, const char* toCity) {
+    try {
+        std::string url = "http://192.168.100.4:8080/flights/fromTo/";
+        url += std::string(fromCity) + "/" + std::string(toCity);
+
+        http::Request request{url};
+        const auto response = request.send("GET");
+
+        char* cstr = new char[response.body.size() + 1];
+        std::strcpy(cstr, std::string(response.body.begin(), response.body.end()).c_str());
+        return cstr;
+    } catch (const std::exception& e) {
+        printf("Request failed, error: %s\n", e.what());
+        return nullptr;
+    }
+}
+extern "C" __attribute__((visibility("default"))) __attribute__((used))
+const char* fetchFlightsFromToWithDateRange(const char* originCity, const char* destinationCity, const char* startDate, const char* endDate) {
+    try {
+        std::string url = "http://192.168.100.4:8080/flights/fromToWithDateRange/";
+        url += std::string(originCity) + "/" + std::string(destinationCity) + "/";
+        url += std::string(startDate) + "/" + std::string(endDate) + "/0"; // Default value for numberOfSeats is 0
+
+        http::Request request{url};
+        const auto response = request.send("GET");
+
+        char* cstr = new char[response.body.size() + 1];
+        std::strcpy(cstr, std::string(response.body.begin(), response.body.end()).c_str());
+        return cstr;
     } catch (const std::exception& e) {
         printf("Request failed, error: %s\n", e.what());
         return nullptr;

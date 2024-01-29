7ef71c4e894052e707c57f3a117539950b501e5d
alpphatra <alpphatra@gmail.com>
2024-01-29 12:36:29 +0100

    httplib.h, config

--- android/cpp/na.cpp
+++ android/cpp/na.cpp
@@ -1,13 +1,30 @@
 #include "HTTPRequest.hpp"
 #include <string>
 #include <exception>
-#include <cstdio>  // Dla printf
+#include <cstdio>
+#include "httplib.h"
+#include "json.hpp"
+
+const std::string BASE_URL = "http://192.168.100.4:8080";
+
+std::string generateURL(const std::string& endpoint, const std::vector<std::string>& parameters = {}) {
+    std::string url = BASE_URL + endpoint;
+
+    for (const std::string& parameter : parameters) {
+        url += "/" + parameter;
+    }
+
+    return url;
+}
 
 extern "C" __attribute__((visibility("default"))) __attribute__((used))
 const char* fetchFlights() {
     try {
-        http::Request request{"http://srv19.mikr.us:20322/flights"};
+        auto url = generateURL("/flights");
+
+        http::Request request{url};
         const auto response = request.send("GET");
+
         char* cstr = new char[response.body.size() + 1];
         std::strcpy(cstr, std::string(response.body.begin(), response.body.end()).c_str());
         return cstr;
@@ -20,17 +37,99 @@ const char* fetchFlights() {
 extern "C" __attribute__((visibility("default"))) __attribute__((used))
 const char* fetchFlightsFromCity(const char* cityName) {
     try {
-        // Tworzenie URL na podstawie nazwy miasta
-        std::string url = "http://srv19.mikr.us:20322/flights/fromCity/" + std::string(cityName);
+        auto url = generateURL("/flights/fromCity", { cityName });
 
-        // Wykonanie zapytania HTTP GET
         http::Request request{url};
         const auto response = request.send("GET");
 
-        // Konwersja odpowiedzi na cstring
         char* cstr = new char[response.body.size() + 1];
         std::strcpy(cstr, std::string(response.body.begin(), response.body.end()).c_str());
         return cstr;
+
+    } catch (const std::exception& e) {
+        printf("Request failed, error: %s\n", e.what());
+        return nullptr;
+    }
+}
+
+extern "C" __attribute__((visibility("default"))) __attribute__((used))
+const char* fetchFlightsFromTo(const char* fromCity, const char* toCity) {
+    try {
+        auto url = generateURL("/flights/fromTo", { fromCity, toCity });
+
+        http::Request request{url};
+        const auto response = request.send("GET");
+
+        char* cstr = new char[response.body.size() + 1];
+        std::strcpy(cstr, std::string(response.body.begin(), response.body.end()).c_str());
+        return cstr;
+    } catch (const std::exception& e) {
+        printf("Request failed, error: %s\n", e.what());
+        return nullptr;
+    }
+}
+
+extern "C" __attribute__((visibility("default"))) __attribute__((used))
+const char* createBooking(int flightId, const char* seatNumber, const char* passengerName) {
+    try {
+        httplib::Client cli("http://192.168.100.4:8080");
+
+        // Utwórz obiekt JSON z danymi rezerwacji
+        nlohmann::json bookingData = {
+                {"flightId", flightId},
+                {"seatNumber", seatNumber},
+                {"passengerName", passengerName}
+        };
+
+        // Wyślij zapytanie POST
+        auto res = cli.Post("/createBooking", bookingData.dump(), "application/json");
+
+        if (res && res->status == 200) {
+            char* cstr = new char[res->body.size() + 1];
+            std::strcpy(cstr, res->body.c_str());
+            return cstr;
+        } else {
+            printf("Booking request failed, status: %d\n", res->status);
+            return nullptr;
+        }
+    } catch (const std::exception& e) {
+        printf("Booking request failed, error: %s\n", e.what());
+        return nullptr;
+    }
+}
+
+extern "C" __attribute__((visibility("default"))) __attribute__((used))
+const char* fetchFlightsFromToWithDateRange(const char* originCity, const char* destinationCity, const char* startDate, const char* endDate) {
+    try {
+        auto url = generateURL("/flights/fromToWithDateRange", { originCity, destinationCity, startDate, endDate }); // Default value for numberOfSeats is 0
+
+        http::Request request{url};
+        const auto response = request.send("GET");
+
+        char* cstr = new char[response.body.size() + 1];
+        std::strcpy(cstr, std::string(response.body.begin(), response.body.end()).c_str());
+        return cstr;
+    } catch (const std::exception& e) {
+        printf("Request failed, error: %s\n", e.what());
+        return nullptr;
+    }
+}
+
+extern "C" __attribute__((visibility("default"))) __attribute__((used))
+const char* fetchReservedSeats() {
+    try {
+        httplib::Client cli(BASE_URL);
+        auto res = cli.Get("/reservedSeats");
+
+        if (res && res->status == 200) {
+            char* cstr = new char[res->body.size() + 1];
+            std::strcpy(cstr, res->body.c_str());
+            return cstr;
+        } else {
+            // Handle error or null response
+            printf("Request failed, status: %d\n", res ? res->status : 0);
+            return nullptr;
+        }
 
     } catch (const std::exception& e) {
         printf("Request failed, error: %s\n", e.what());
--- android/cpp/na.hpp
+++ android/cpp/na.hpp
@@ -8,14 +8,4 @@
 #ifndef na_hpp
 #define na_hpp
 
-#include <string>
-
-extern "C" __attribute__((visibility("default"))) __attribute__((used)) {
-// Deklaracja funkcji fetchFlights
-const char* fetchFlights();
-
-// Deklaracja funkcji fetchFlightsFromCity
-const char* fetchFlightsFromCity(const char* cityName);
-}
-
 #endif /* na_hpp */
